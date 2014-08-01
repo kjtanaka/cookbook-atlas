acf6122006c5b205f1c2f9e76c121a82730801be
Koji Tanaka <kj.tanaka@gmail.com>
2014-08-01 14:43:28 -0400

    adding support of rhel 5

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,12 @@ default['atlas']['download_dir'] = "/root/source"
 default['atlas']['install_dir'] = "/opt"
 default['atlas']['lapack_version'] = "3.5.0"
 default['atlas']['lapack_download_url'] = "http://www.netlib.org/lapack/lapack-#{node['atlas']['lapack_version']}.tgz"
-default['atlas']['required_packages'] = %w[gcc-gfortran rsync cpufrequtils]
+
+if platform_family == "rhel" and platform_version.to_f >= 6 and platform_version.to_f < 7 then
+  default['atlas']['required_packages'] = %w[gcc-gfortran rsync cpufrequtils]
+elsif platform_family == "rhel" and platform_version.to_f >= 5 and platform_version.to_f < 6 then
+  default['atlas']['required_packages'] = %w[gcc-gfortran rsync cpufreq-utils]
+end
 
 # Environment Modules
 default['atlas']['modulefile_dir'] = "/opt/modules-3.2.10/Modules/3.2.10/modulefiles/atlas"
